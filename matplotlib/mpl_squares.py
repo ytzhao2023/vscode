13dc8b6a1c2330d2a8f67134cc1c2524bf80028f
ytzhao <ytzhao2022@gmail.com>
2023-09-12 14:51:26 -0700

    second change

--- matplotlib/mpl_squares.py
+++ matplotlib/mpl_squares.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares, linewidth = 5)
+plt.plot(input_values, squares, linewidth = 5)
 
 # Setting the title of the diagram, and adding labels to the coordinate axis.
 plt.title("Square Numbers", fontsize = 24)
